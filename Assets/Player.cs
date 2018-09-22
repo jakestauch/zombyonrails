3867eb2c97daedf811dccc8cf9e0bb6d8707546c
Jake Stauch <jake@neuro.plus>
2018-09-22 12:10:16 -0400

    Player translation

---
+++ Assets/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip ("In ms^-1")][SerializeField] float xSpeed = 10f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 10f;
+
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+
+    [SerializeField] float pitchStrength = -5f;
+
+
+    // Use this for initialization
+    void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        Vector3 currentPosition = transform.localPosition;
+
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float rawNewXPos = currentPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        float rawNewYPos = currentPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, currentPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * pitchStrength;
+        float yaw = 0f;
+        float roll = 0f;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+} 
