0081c04b47c6eb42d9052b988ec5b9393f391951
Jake Stauch <jake@neuro.plus>
2018-10-01 18:18:27 -0400

    Fin!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -85,28 +85,31 @@ public class PlayerController : MonoBehaviour
     {
         if (CrossPlatformInputManager.GetButton("Fire"))
         {
-            ActivateGuns();
+            SetGunsActive(true);
         }
         else
         {
-            DeactivateGuns();
+            SetGunsActive(false);
         }
 
     }
 
-    private void ActivateGuns()
+    private void SetGunsActive(bool isActive)
     {
         foreach (GameObject gun in guns)
         {
-            gun.SetActive(true);
-        }
-    }
-
-    private void DeactivateGuns()
-    {
-        foreach (GameObject gun in guns)
-        {
-            gun.SetActive(false);
+            var emissionModule = gun.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
+            if (isActive)
+            {
+                AudioSource gunSound = GetComponent<AudioSource>();
+                gunSound.enabled = true;
+            }
+            else
+            {
+                AudioSource gunSound = GetComponent<AudioSource>();
+                gunSound.enabled = false;
+            }
         }
     }
 }
